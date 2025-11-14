d24b37ce84a366ffbf6362a26ddac1d56d8042de
sierzegao <sierzegao@msoe.edu>
2025-11-14 10:37:09 -0600

    Initial Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DubiousDuel
+
 For our awesome battle sim
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>Twin where have you been</p>
+      <p>6 7</p>
+    </div>
+  );
+}
+
+export default App;
