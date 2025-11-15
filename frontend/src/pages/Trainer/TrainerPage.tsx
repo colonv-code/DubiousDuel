c1b8f8f6715a3b7d5031e5b9a7ba5bf06a57517e
sierzegao <sierzegao@msoe.edu>
2025-11-15 13:01:19 -0600

    WIP trainer page

--- frontend/src/pages/Trainer/TrainerPage.tsx
+++ frontend/src/pages/Trainer/TrainerPage.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useMemo, useState } from "react";
 import type { Trainer } from "../../api/trainer.api";
+import { getPokemon, type Pokemon } from "../../api/pokemon.api";
+import "./TrainerPage.css";
 
 export interface TrainerPageProps {
   trainer: Trainer | null;
@@ -6,9 +9,63 @@ export interface TrainerPageProps {
 }
 
 export function TrainerPage({ trainer }: TrainerPageProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+
+  const [searchInput, setSearchInput] = useState<string>("");
+  const filteredList = useMemo(
+    () =>
+      pokemonList.filter(
+        (pokemon) =>
+          pokemon.Name.toLocaleLowerCase().includes(
+            searchInput.toLocaleLowerCase()
+          ) ||
+          pokemon.Id.toLocaleLowerCase().includes(
+            searchInput.toLocaleLowerCase()
+          )
+      ),
+    [pokemonList, searchInput]
+  );
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPokemon().then((res) => {
+      setPokemonList(res);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const createPokemonRowClickHandler = (pokemon: Pokemon) => () => {
+    // todo
+  };
+
   return (
-    <div>
-      <p>Hello, {trainer?.username}!</p>
+    <div className="trainerPage">
+      <div className="teamBuilder">
+        <h1>Welcome, Trainer {trainer?.username}!</h1>
+      </div>
+      <div className="pokemonSidebar">
+        <input
+          type="text"
+          placeholder="search"
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="scrollContainer">
+            {filteredList.map((pokemon) => (
+              <p
+                className="pokemonRow"
+                onClick={createPokemonRowClickHandler(pokemon)}
+                key={pokemon.Id}
+              >
+                {pokemon.Id} - {pokemon.Name}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
