326ee71d935d96372dd69b9b5990451438df4e6b
sierzegao <sierzegao@msoe.edu>
2025-11-26 22:45:30 -0600

    WIP battle modal: display moves and team

--- frontend/src/pages/Battle/BattleModal.tsx
+++ frontend/src/pages/Battle/BattleModal.tsx
@@ -1,4 +1,5 @@
 import type { Battle } from "../../api/battle.api";
+import type { Pokemon } from "../../api/pokemon.api";
 import type { Trainer } from "../../api/trainer.api";
 import { Modal } from "../../components/Modal";
 import { nameToImageUri } from "../../helpers/nameToImageUri";
@@ -8,7 +9,11 @@ export interface BattleModalProps {
   visible: boolean;
   battle: Battle;
   trainer: Trainer;
+  selectedMoveName: string | null;
+  selectedPokemonIndex: number | null;
   onClose: () => void;
+  onMoveSelected: (moveName: string) => void;
+  onPokemonSelected: (pokemonIndex: number) => void;
   onTurnComplete: () => void;
 }
 
@@ -16,7 +21,11 @@ export function BattleModal({
   visible,
   battle,
   trainer,
+  selectedMoveName,
+  selectedPokemonIndex,
   onClose,
+  onMoveSelected,
+  onPokemonSelected,
   onTurnComplete,
 }: BattleModalProps) {
   if (!visible) return null;
@@ -31,27 +40,97 @@ export function BattleModal({
       battle.trainer1 === trainer.username) ||
     (battle.status === "trainer2turn" && battle.trainer2 === trainer.username);
 
+  const yourPokemon =
+    battle.trainer1 === trainer.username ? pokemon1 : pokemon2;
+  const yourPokemonIndex =
+    battle.trainer1 === trainer.username
+      ? latestTurn.pokemon1
+      : latestTurn.pokemon2;
+
+  // filter out moves that have Power of "--" or "??"
+  // these are usually status moves that don't do damage
+  // our UI only shows damaging moves for now
+  const filteredMoves = Object.entries(yourPokemon.Moves).filter(
+    ([, move]) => move.Power !== "--" && move.Power !== "??"
+  );
+
+  const yourTeam =
+    battle.trainer1 === trainer.username
+      ? battle.trainer1team
+      : battle.trainer2team;
+
+  const createMoveSelectedHandler = (moveName: string) => () => {
+    onMoveSelected(moveName);
+  };
+
+  const createPokemonSelectedHandler =
+    (pokemon: Pokemon, pokemonIndex: number) => () => {
+      if (pokemon === yourPokemon) return; // can't select the active pokemon
+      onPokemonSelected(pokemonIndex);
+    };
+
   return (
     <Modal visible={visible} onClose={onClose}>
-      <div className="battleModalContainer">
-        <p>
-          {battle.trainer1} vs. {battle.trainer2}
-        </p>
-        {isYourTurn && <p>Your turn! Choose a move or switch your pokemon.</p>}
-        <div className="battlePokemonContainer">
-          <div style={{ flexDirection: "column" }}>
-            <img
-              src={nameToImageUri(pokemon1.Name)}
-              alt={pokemon1 ? pokemon1.Name : "No Pokemon"}
-            />
-            <p>HP: {latestTurn.team1status[latestTurn.pokemon1].hp}</p>
+      <div
+        className={`battleModalContainer ${
+          // Determine which side the trainer is on and display accordingly
+          battle.trainer1 === trainer.username
+            ? "battleModalTrainer1"
+            : "battleModalTrainer2"
+        }`}
+      >
+        <div className="battleSideContainer">
+          <p>{yourPokemon.Name}'s Moves</p>
+          <div className="movesContainer">
+            {filteredMoves.map(([moveName, move]) => (
+              <div
+                className={`moveRow ${
+                  selectedMoveName === moveName ? "selectedMove" : ""
+                }`}
+                key={moveName}
+                onClick={createMoveSelectedHandler(moveName)}
+              >
+                {moveName}: {move.Power}
+              </div>
+            ))}
           </div>
-          <div style={{ flexDirection: "column" }}>
-            <img
-              src={nameToImageUri(pokemon2.Name)}
-              alt={pokemon2 ? pokemon2.Name : "No Pokemon"}
-            />
-            <p>HP: {latestTurn.team2status[latestTurn.pokemon2].hp}</p>
+          <p>Your Team</p>
+          <div className="teamContainer">
+            {yourTeam.map((pokemon, index) => (
+              <img
+                src={nameToImageUri(pokemon.Name)}
+                className={`teamPokemon ${
+                  selectedPokemonIndex === index ? "selectedPokemon" : ""
+                } 
+                ${yourPokemonIndex === index ? "activePokemon" : ""}`}
+                onClick={createPokemonSelectedHandler(pokemon, index)}
+                alt={pokemon ? pokemon.Name : "No Pokemon"}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="battleCenterContainer">
+          <p>
+            {battle.trainer1} vs. {battle.trainer2}
+          </p>
+          {isYourTurn && (
+            <p>Your turn! Choose a move or switch your pokemon.</p>
+          )}
+          <div className="battlePokemonContainer">
+            <div style={{ flexDirection: "column" }}>
+              <img
+                src={nameToImageUri(pokemon1.Name)}
+                alt={pokemon1 ? pokemon1.Name : "No Pokemon"}
+              />
+              <p>HP: {latestTurn.team1status[latestTurn.pokemon1].hp}</p>
+            </div>
+            <div style={{ flexDirection: "column" }}>
+              <img
+                src={nameToImageUri(pokemon2.Name)}
+                alt={pokemon2 ? pokemon2.Name : "No Pokemon"}
+              />
+              <p>HP: {latestTurn.team2status[latestTurn.pokemon2].hp}</p>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/pages/Battle/useBattleModal.tsx
+++ frontend/src/pages/Battle/useBattleModal.tsx
@@ -10,12 +10,31 @@ export function useBattleModal(
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const [battle, setBattle] = useState<Battle | null>(null);
 
+  const [selectedMoveName, setSelectedMoveName] = useState<string | null>(
+    "Ice Punch"
+  );
+  const [selectedPokemonIndex, setSelectedPokemonIndex] = useState<
+    number | null
+  >(null);
+
+  const handleMoveSelected = (moveName: string) => {
+    setSelectedMoveName(moveName);
+    setSelectedPokemonIndex(null);
+  };
+
+  const handlePokemonSelected = (pokemonIndex: number) => {
+    setSelectedPokemonIndex(pokemonIndex);
+    setSelectedMoveName(null);
+  };
+
   const openModal = (battleToAccept: Battle) => {
     setBattle(battleToAccept);
     setIsVisible(true);
   };
 
   const handleClose = () => {
+    setSelectedMoveName(null);
+    setSelectedPokemonIndex(null);
     setBattle(null);
     setIsVisible(false);
   };
@@ -33,6 +52,10 @@ export function useBattleModal(
         battle={battle!}
         trainer={trainer}
         visible={isVisible}
+        selectedMoveName={selectedMoveName}
+        selectedPokemonIndex={selectedPokemonIndex}
+        onMoveSelected={handleMoveSelected}
+        onPokemonSelected={handlePokemonSelected}
         onClose={handleClose}
         onTurnComplete={handleTurnComplete}
       />
