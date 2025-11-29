113f9c3cec76ef97de9213978a6a84afc9a8b935
sierzegao <sierzegao@msoe.edu>
2025-11-28 18:13:09 -0600

    Battle turn endpoint. If god wills it it will be.

--- backend/models.ts
+++ backend/models.ts
@@ -68,3 +68,14 @@ export interface Battle {
   trainer2team: Pokemon[];
   turns: BattleTurn[];
 }
+
+// For when a turn is sent from frontend to backend
+// the status of the team will be calculated on the backend
+// based on the moves used and other factors
+// so that the frontend cannot calculate it incorrectly or cheat
+export interface BattleTurnRequest {
+  movingTrainer: 1 | 2; // 1 for trainer1, 2 for trainer2
+  moveUsed: string | null; // move used by the active pokemon, null on first or switch out pokemon
+  pokemon1: number; // index of pokemon in trainer1team
+  pokemon2: number; // index of pokemon in trainer2team
+}
--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,13 @@ import express, { Request, Response } from "express";
 import { MongoClient, ObjectId, Collection } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Battle, Pokemon, Trainer } from "./models";
+import {
+  Battle,
+  BattleTurn,
+  BattleTurnRequest,
+  Pokemon,
+  Trainer,
+} from "./models";
 
 dotenv.config({ path: "../.env.local" });
 
@@ -146,6 +152,131 @@ app.put("/battles/:battleId/accept", async (req: Request, res: Response) => {
         team2status: trainer.team.map((p: Pokemon) => ({ hp: parseInt(p.HP) })),
       },
     ],
+  };
+
+  await battles.updateOne(
+    { _id: new ObjectId(battleId) },
+    { $set: updatedBattle }
+  );
+  res.json(updatedBattle);
+});
+
+// process a turn in a battle
+// body is the turn data
+app.put("/battles/:battleId/turn", async (req: Request, res: Response) => {
+  const battleId = req.params.battleId;
+  const { turnRequest } = req.body as { turnRequest: BattleTurnRequest };
+
+  if (!battleId) {
+    return res.status(400).json({ error: "battleId is required" });
+  }
+  if (!turnRequest) {
+    return res.status(400).json({ error: "turn data is required" });
+  }
+  const battle = await battles.findOne({ _id: new ObjectId(battleId) });
+
+  if (!battle) {
+    return res.status(404).json({ error: "battle not found" });
+  }
+
+  // validate turn is correct based on battle status
+  const expectedMovingTrainer =
+    battle.status === "trainer1turn"
+      ? 1
+      : battle.status === "trainer2turn"
+      ? 2
+      : 0;
+
+  if (turnRequest.movingTrainer !== expectedMovingTrainer) {
+    return res.status(400).json({ error: "not this trainer's turn to move" });
+  }
+
+  const previousTurn = battle.turns[battle.turns.length - 1];
+
+  // if moveUsed is null, or pokemon fainted previous turn, it's a switch
+  // validate that the pokemon being switched to is different from the current one
+  // AND ALSO NOT FAINTED
+  const noMoveUsed = turnRequest.moveUsed === null;
+  const pokemonJustFainted =
+    turnRequest.movingTrainer === 1
+      ? previousTurn.team1status[turnRequest.pokemon1].hp <= 0
+      : previousTurn.team2status[turnRequest.pokemon2].hp <= 0;
+
+  if (noMoveUsed || pokemonJustFainted) {
+    const isSwitchedToSamePokemon =
+      turnRequest.movingTrainer === 1
+        ? turnRequest.pokemon1 === previousTurn.pokemon1
+        : turnRequest.pokemon2 === previousTurn.pokemon2;
+    const isSwitchedToFaintedPokemon =
+      turnRequest.movingTrainer === 1
+        ? previousTurn.team1status[turnRequest.pokemon1].hp <= 0
+        : previousTurn.team2status[turnRequest.pokemon2].hp <= 0;
+
+    if (isSwitchedToSamePokemon) {
+      return res
+        .status(400)
+        .json({ error: "cannot switch to the same pokemon" });
+    }
+    if (isSwitchedToFaintedPokemon) {
+      return res
+        .status(400)
+        .json({ error: "cannot switch to a fainted pokemon" });
+    }
+  }
+
+  // these are the status arrays that will be updated
+  // based on the turn actions (just attacks for now, so only hp changes)
+  const team1NewStatus = [...previousTurn.team1status];
+  const team2NewStatus = [...previousTurn.team2status];
+
+  if (turnRequest.moveUsed !== null) {
+    // validate that the move exists on the pokemon
+    const pokemon1 = battle.trainer1team[turnRequest.pokemon1];
+    const pokemon2 = battle.trainer2team[turnRequest.pokemon2];
+    const moveUsed =
+      turnRequest.movingTrainer === 1
+        ? pokemon1.Moves[turnRequest.moveUsed]
+        : pokemon2.Moves[turnRequest.moveUsed];
+    if (!moveUsed) {
+      return res.status(400).json({ error: "invalid move used" });
+    }
+
+    // DAMAGE CALCULATION UPDATE LATER
+    const damage = parseInt(moveUsed.Power) || 10;
+    // DAMAGE CALULATION UPDATE LATER
+
+    if (turnRequest.movingTrainer === 1) {
+      const targetHp = team2NewStatus[turnRequest.pokemon2].hp - damage;
+      team2NewStatus[turnRequest.pokemon2].hp = Math.max(targetHp, 0);
+    } else {
+      const targetHp = team1NewStatus[turnRequest.pokemon1].hp - damage;
+      team1NewStatus[turnRequest.pokemon1].hp = Math.max(targetHp, 0);
+    }
+  }
+
+  // update battle status
+  const team1AllFainted = team1NewStatus.every((status) => status.hp <= 0);
+  const team2AllFainted = team2NewStatus.every((status) => status.hp <= 0);
+
+  const newStatus = team1AllFainted
+    ? "trainer2win"
+    : team2AllFainted
+    ? "trainer1win"
+    : battle.status === "trainer1turn"
+    ? "trainer2turn"
+    : "trainer1turn";
+
+  const updatedTurn: BattleTurn = {
+    turnNumber: previousTurn.turnNumber + 1,
+    ...turnRequest,
+    team1status: team1NewStatus,
+    team2status: team2NewStatus,
+  };
+
+  const updatedBattle: Battle = {
+    ...battle,
+    status: newStatus,
+    turns: [...battle.turns, updatedTurn],
   };
 
   await battles.updateOne(
